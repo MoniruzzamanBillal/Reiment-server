ca2736061efde1da2eccc5a211e6c0a4e89f6c33
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-11-23 19:07:53 +0600

    product review added

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,9 +1,31 @@
+import { checkDataExist } from "../../util/CheckDataExist";
+import { orderModel } from "../order/order.model";
+import { productModel } from "../product/product.model";
+import { userModel } from "../User/user.model";
 import { TReview } from "./review.interface";
 import { reviewModel } from "./review.model";
 
 // ! for giving review
 const addReview = async (payload: TReview) => {
-  console.log(payload);
+  const { userId, productId, purchaseId } = payload;
+
+  await checkDataExist(userModel, userId, "This user doesn't exist !!");
+
+  await checkDataExist(
+    productModel,
+    productId,
+    "This product doesn't exist !!"
+  );
+
+  await checkDataExist(
+    orderModel,
+    purchaseId,
+    "This purchase id doesn't exist !!"
+  );
+
+  const result = await reviewModel.create(payload);
+
+  return result;
 };
 
 // ! for getting all review
